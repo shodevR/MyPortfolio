15d54caa305effc53d561b4612444c01cee91ccf
shodevR <royshodev@gmail.com>
2023-05-10 22:12:51 +0530

    update from io

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 
@@ -60,66 +59,3 @@ window.addEventListener("scroll", function () {
 });
 
 
-=======
-'use strict';
-
-
-
-/**
- * add event on element
- */
-
-const addEventOnElem = function (elem, type, callback) {
-  if (elem.length > 1) {
-    for (let i = 0; i < elem.length; i++) {
-      elem[i].addEventListener(type, callback);
-    }
-  } else {
-    elem.addEventListener(type, callback);
-  }
-}
-
-
-
-/**
- * toggle navbar
- */
-
-const navbar = document.querySelector("[data-navbar]");
-const navLinks = document.querySelectorAll("[data-nav-link]");
-let navToggler = document.querySelector("[data-nav-toggler]");
-
-let toggleNavbar = function () {
-  
-navbar.classList.toggle("active");
-navToggler.classList.toggle("active");
-  
-}
-
-addEventOnElem(navToggler, "click", toggleNavbar);
-
-const closeNavbar = function () {
-  navbar.classList.remove("active");
-  navToggler.classList.remove("active");
-}
-
-addEventOnElem(navLinks, "click", closeNavbar);
-
-
-
-/**
- * header active
- */
-
-const header = document.querySelector("[data-header]");
-
-window.addEventListener("scroll", function () {
-  if (window.scrollY > 100) {
-    header.classList.add("active");
-  } else {
-    header.classList.remove("active");
-  }
-});
-
-
->>>>>>> 71fe24c21ed926a517b2f811279673ce1348f7e8
